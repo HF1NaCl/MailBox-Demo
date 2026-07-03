78df337a3926faad3d3f38602e2a67bebd7386c2
HF1NaCl <enrifuoro@gmail.com>
2026-07-03 00:55:00 -0400

    Testing Pages

---
+++ docs/assets/p-D7bSXQD3-qlO9RyrS.js
@@ -0,0 +1,3 @@
+import{O as s,P as r,Q as a,R as i,S as m}from"./index-DcJXtGot.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const d=()=>{const e=window;e.addEventListener("statusTap",()=>{s(()=>{const n=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!n)return;const t=r(n);t&&new Promise(o=>a(t,o)).then(()=>{i(async()=>{t.style.setProperty("--overflow","hidden"),await m(t,300),t.style.removeProperty("--overflow")})})})})};export{d as startStatusTap};
