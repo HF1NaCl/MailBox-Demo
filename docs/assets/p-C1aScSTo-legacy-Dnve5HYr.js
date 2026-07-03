7cee410d90e691d28bd42694ae59101230cce461
HF1NaCl <enrifuoro@gmail.com>
2026-07-03 00:57:45 -0400

    Test Pages 2

--- docs/assets/p-C1aScSTo-legacy-Dnve5HYr.js
+++ docs/assets/p-C1aScSTo-legacy-Dnve5HYr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D4n6E0Ff.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.K,n=t.L}],execute:function(){
+System.register(["./index-legacy-C_lP5PO1.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.K,n=t.L}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
